0950a06bfca8c20a4df362405889b8846c45c254
Abdullahi <muxsinxareed6@gmail.com>
2023-10-11 12:12:29 +0200

    Time converter

--- main.java
+++ main.java
@@ -26,7 +26,28 @@ public class main {
         int remainder = täljäre % nämnare;
         System.out.println("Talet "  + täljäre + ("/") + nämnare +" blir " + divide + " " + remainder + "/" + nämnare);
 
+       // Uppgift omvandla till sekunder
+
+       System.out.print("Ange antalet timmar: ");
+       double hours = sc.nextInt();
+
+       System.out.print("Ange antalet minuter: ");
+       double minutes = sc.nextInt();
+
+       System.out.print("Ange antalet sekunder: ");
+       double seconds = sc.nextInt();
+
+       double timmar = hours + seconds / 360 + minutes / 60;
+       double minuter = hours * 60 + seconds / 60 + minutes;
+       double sekunder = timmar * 360 + minuter * 60 + seconds;
        
+       System.out.println("Tidomvandlingen ger : " + timmar + "h" + " = " + minuter + " min " + " = " + sekunder);
+
+    //    double toHours = hours + (minutes / 60) + seconds / 360;
+
+    //    System.out.println("Tidsomvadling ger : " + toHours + "h");
+
+
 
 
     }
